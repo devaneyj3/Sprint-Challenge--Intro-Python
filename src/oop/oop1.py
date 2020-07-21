ed03f18c21e19e801786263f42e782f682de458a
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-07-21 15:52:44 -0400

    pass cityreader

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -11,8 +11,43 @@
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
 #
-# e.g.
-#
+# This is the base class
+class Vehicle:
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Base Class
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+        
+    # Airplane's parent is Flight Vehicle
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Base Class
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
 # class Whatever:
 #     pass
 #
